3f2b8ed1499f6635216dc61b8449d2c7cac50b19
Levan <echolevan@gmail.com>
2017-09-06 15:03:43 +0800

    初始化配置

---
+++ src/components/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    hhh
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {}
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'    // 使用 CSS
+import 'common/stylus/index.styl'       // rest css
+Vue.use(iView)
+Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start()
+  next()
+})
+
+router.afterEach((to, from, next) => {
+  iView.LoadingBar.finish()
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
